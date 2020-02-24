74ef2fb689f42723d9d5b32dc240dbc2ddc14ade
kxelhua <kxelhua@tywls.org>
2020-02-24 18:38:01 -0500

    added state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import '../css/App.css';
  import Question from './Question';
+ import {buildFirebase} from '../clients/firebase.js';
 
 class App extends Component {
+  constructor (props) {
+    super (props);
+    this.state={
+        choices: [
+            "48",
+            "13",
+            "52",
+            "50"
+        ],
+        correct_choice_index: 3,
+        question_text: "How many states are in the United States?",
+    };
+  }
 
   render() {
     return (
       <div className="app">
         <Question questiontext ="How many states are in the United States?"/>
-        
       </div>
     );
   }
